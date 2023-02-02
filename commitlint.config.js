7d4f3306c3d74baabcce532ce3f1942aa1a79338
zrcoy <zhangruichaotyler@gmail.com>
2023-02-02 16:42:17 -0500

    ci: test commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,6 +7,7 @@
 // refactor: A code change that neither fixes a bug nor adds a feature
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
+// to be deleted: test for commit lint
 
 module.exports = {
   extends:['@commitlint/config-conventional'],
